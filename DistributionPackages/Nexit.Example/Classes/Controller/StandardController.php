17a3787dff1dadfd42b42030b314a54213a2c7b4
Marius Preuss <Marius.Preuss@internezzo.ch>
2023-02-01 13:15:31 +0100

    Add routes for package

--- DistributionPackages/Nexit.Example/Classes/Controller/StandardController.php
+++ DistributionPackages/Nexit.Example/Classes/Controller/StandardController.php
@@ -11,14 +11,18 @@ use Neos\Flow\Mvc\Controller\ActionController;
 class StandardController extends ActionController
 {
 
+    protected $defaultViewObjectName = \Neos\Flow\Mvc\View\JsonView::class;
+
+    public function showAction()
+    {
+        $this->view->assign('value', 'foobar');
+    }
 
     /**
      * @return void
      */
     public function indexAction()
     {
-        $this->view->assign('foos', array(
-            'bar', 'baz'
-        ));
+        $this->view->assign('value', 'foobar');
     }
 }
